aa06912145040a39ef8bb118ba38a43283bb8bcf
mohammad.khan <mohammad.khan@telekom-digital.com>
2024-03-07 15:41:56 +0530

    add imports for all steps

--- src/main/kotlin/com/telekom/iaplatformcli/generate/sourcecode/LmosImports.kt
+++ src/main/kotlin/com/telekom/iaplatformcli/generate/sourcecode/LmosImports.kt
@@ -6,20 +6,44 @@ interface LmosImports {
     fun getLmosImports(): List<String>
 }
 @Component
-class KotlinLmosImports : LmosImports{
+class KotlinLmosImports : LmosImports {
 
-    companion object{
+    companion object {
         const val LMOS_BOOT_PROPERTIES = "import com.telekom.lmos.boot.KernelProperties"
-        const val LMOS_KERNEL_TENANT="import com.telekom.lmos.kernel.tenant.TenantProvider"
-        const val LMOS_KERNEL_STEPS = "import com.telekom.lmos.kernel.steps.*"
-        const val LMOS_KERNEL_AGENT="import com.telekom.lmos.kernel.agent.Agent\n" +
+        const val LMOS_KERNEL_TENANT = "import com.telekom.lmos.kernel.tenant.TenantProvider"
+        const val LMOS_KERNEL_STEPS = "import com.telekom.lmos.kernel.steps.*" +
+                "import com.telekom.lmos.kernel.UnknownTenantException"
+        const val LMOS_KERNEL_AGENT = "import com.telekom.lmos.kernel.agent.Agent\n" +
                 "import com.telekom.lmos.kernel.agent.AgentProfile"
-        const val LMOS_KERNEL_USER="import com.telekom.lmos.kernel.user.UserInformation\n" +
+        const val LMOS_KERNEL_USER = "import com.telekom.lmos.kernel.user.UserInformation\n" +
                 "import com.telekom.lmos.kernel.user.UserProvider"
         const val SPRING_BOOT_IMPORTS = "import org.springframework.stereotype.Component"
+        const val LOGGER_IMPORTS = "import org.slf4j.LoggerFactory"
+        const val PLATFORM_IMPORTS = "import com.telekom.ia.platform.assistants.anonymization.steps.Anonymize\n" +
+                "import com.telekom.ia.platform.assistants.anonymization.steps.Deanonymize\n" +
+                "import com.telekom.ia.platform.assistants.billing.BillingAgent\n" +
+                "import com.telekom.ia.platform.assistants.faq.FAQAgent\n" +
+                "import com.telekom.ia.platform.assistants.faq.steps.*\n" +
+                "import com.telekom.ia.platform.assistants.steps.HandleAuthResult\n" +
+                "import com.telekom.ia.platform.assistants.steps.LegacySaveResponse\n" +
+                "import com.telekom.ia.platform.assistants.steps.LogFinalResponse\n" +
+                "import com.telekom.ia.platform.assistants.steps.SaveResponse\n" +
+                "import com.telekom.ia.platform.assistants.steps.*\n" +
+                "import com.telekom.ia.platform.inbound.DialogEvent\n" +
+                "import com.telekom.ia.platform.inbound.withLogEventContext"
+
     }
 
     override fun getLmosImports(): List<String> {
-        return listOf(LMOS_BOOT_PROPERTIES, LMOS_KERNEL_TENANT, LMOS_KERNEL_STEPS, LMOS_KERNEL_USER, LMOS_KERNEL_AGENT, SPRING_BOOT_IMPORTS);
+        return listOf(
+            LMOS_BOOT_PROPERTIES,
+            PLATFORM_IMPORTS,
+            LMOS_KERNEL_TENANT,
+            LMOS_KERNEL_STEPS,
+            LMOS_KERNEL_USER,
+            LMOS_KERNEL_AGENT,
+            SPRING_BOOT_IMPORTS,
+            LOGGER_IMPORTS
+        )
     }
 }
