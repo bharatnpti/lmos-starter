3417f252ebfb843a70aca97c0ac6449f462accbf
mohammad.khan <mohammad.khan@telekom-digital.com>
2024-03-26 12:15:31 +0530

    style: text formatting for project generation steps

--- lmos-cli-script.sh
+++ lmos-cli-script.sh
@@ -39,6 +39,7 @@ fi
 # Execute jar with parameters
 java -jar ./build/libs/lmos-cli.jar --dirName "$project_dir" --agentName "$agent_name" --packageName "$package_name" --steps "$steps" --projectName "$project_name"
 
+echo -e "\033[1m                    GENERATED NEW PROJECT: $project_name  \033[0m"
 
 new_proj_dir="${project_dir}/${project_name}"
 
@@ -47,11 +48,15 @@ cd "$new_proj_dir" || { echo "Failed to change directory"; exit 1; }
 
 echo "changed the working directory to ${PWD}"
 echo "new proj name is: $new_proj_dir , and pwd is ${PWD}"
+
+echo -e "\033[1m                    BUILDING PROJECT: $project_name \033[0m"
+
 # Build project using gradle
 ./gradlew clean build
 
 cd "build/libs" || { echo "Failed to change to build directory for generated project"; exit 1; }
 
+echo -e "\033[1m                    STARTING APPLICATION: $project_name \033[0m"
 # Execute "$project_name.jar"
 java -jar "$project_name.jar"
 
